1b29f8e625b85e32b0f3b38af1347f62438f568e
suckitfa <tangbob171@gmail.com>
2024-04-21 09:30:48 +0800

    feat: 使用独立文件维护webpack的配置

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # learn-webpack
+
+#### webpack的默认入口 src/index.js, 默认打包出口/dist
+#### “执行npm run build，Webpack就会预先读取webpack.config.js，然后进行打包。”
--- src/add-content.js
+++ src/add-content.js
@@ -1,4 +1,5 @@
 export default function() {
     document.write('Hello world!');
     document.write("I am using npm scripts!\n")
+    document.write('I am using webpack app!<br/>')
 }
